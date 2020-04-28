2563386f78e72da6cbfd213b35a7e5ae33af3ef8
Xons001 <sean.saez.f@gmail.com>
2020-04-28 17:23:07 +0200

    Consulta clientes

--- consultasInsert.js
+++ consultasInsert.js
@@ -11,8 +11,23 @@ const pool = new Pool({
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  
+    
+    res.send('Hello World!');
 });
+
+.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM cliente');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
 
 app.listen(PORT, function () {
   console.log('Example app listening on port 5000!');
