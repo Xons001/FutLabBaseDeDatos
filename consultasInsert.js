96eaac73d38c49a1f3af27e09975677162448a10
Xons001 <sean.saez.f@gmail.com>
2020-04-28 14:23:18 +0200

    primer commit

---
+++ consultasInsert.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const PORT = process.env.PORT || 5000
+const bodyParser = require('body-parser');
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, function () {
+  console.log('Example app listening on port 4000!');
+});
